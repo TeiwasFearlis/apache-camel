753d3459463db518838cea84fb9b8f5998dcb848
Zaitsev Anton <teiwas88@rambler.ru>
2021-12-03 17:54:19 +0300

    work bd part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,19 +19,19 @@ repositories {
 dependencies {
 	implementation("org.apache.camel.springboot:camel-spring-boot-starter:3.13.0")
 
-	// implementation("org.springframework.boot:spring-boot-starter-actuator")
-	// implementation("org.springframework.boot:spring-boot-starter-aop")
-	// implementation("org.apache.camel:camel-core:3.4.2")
-	// implementation("org.apache.camel:camel-context:2.23.2")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	runtimeOnly("org.postgresql:postgresql")
 	implementation("org.springframework:spring-jdbc")
+
 	implementation("org.liquibase:liquibase-core:4.4.3")
+
 	implementation("org.apache.camel:camel-jdbc:3.4.2")
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/ru/apachecamel/Starter.kt
+++ src/main/kotlin/ru/apachecamel/Starter.kt
@@ -1,17 +1,34 @@
 package ru.apachecamel
 
 import org.apache.camel.builder.RouteBuilder
-import org.apache.camel.impl.DefaultCamelContext
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import javax.annotation.processing.Processor
+import javax.sql.DataSource
 
 @Component
 class Starter : RouteBuilder(){
 
+    @Autowired
+    lateinit var dataSource: DataSource
+    
+    // override fun configure() {
+    //     from("file:/Users/azaitsev/Downloads/apache-camel/files/input")
+    //         .routeId("traveling")
+    //         .log("\${headers.CamelFileName}")
+    //         .log(">>>>>>>> \${body}")
+    //         .to("file:/Users/azaitsev/Downloads/files/output")
+    //         }
+
     override fun configure() {
-        from("file:/Users/azaitsev/Downloads/apache-camel/src/main/files/from")
+       // regist()
+        from("timer:base?period=30000")
             .routeId("traveling")
-            .log("\${headers.CamelFileName}")
+            .setBody(simple("INSERT INTO age_table(age) values(18)"))
+            //.log("\${headers.CamelFileName}")
+            .to("jdbc:dataSource")
+            .setBody(simple("select * from age_table"))
+            .to("jdbc:dataSource")
             .log(">>>>>>>> \${body}")
-            .to("file:/Users/azaitsev/Downloads/apache-camel/src/main/files/toA")
             }
 }
