30092cbe19f9b8f3db4b15b90a1f3be960594de3
Zaitsev Anton <teiwas88@rambler.ru>
2021-12-06 17:59:04 +0300

    add module jackson kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,10 @@ dependencies {
 
 	implementation("org.apache.camel:camel-jdbc:3.4.2")
 
+	 implementation("org.apache.camel:camel-jackson:3.13.0")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/ru/apachecamel/Starter.kt
+++ src/main/kotlin/ru/apachecamel/Starter.kt
@@ -1,17 +1,22 @@
 package ru.apachecamel
 
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.component.jackson.JacksonDataFormat
+import org.apache.camel.model.dataformat.JsonLibrary
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
-import javax.annotation.processing.Processor
-import javax.sql.DataSource
 
 @Component
 class Starter : RouteBuilder(){
 
     @Autowired
-    lateinit var dataSource: DataSource
-    
+    lateinit var jdbcTemplate: JdbcTemplate
+
+
     // override fun configure() {
     //     from("file:/Users/azaitsev/Downloads/apache-camel/files/input")
     //         .routeId("traveling")
@@ -20,15 +25,54 @@ class Starter : RouteBuilder(){
     //         .to("file:/Users/azaitsev/Downloads/files/output")
     //         }
 
+    // override fun configure() {
+    //    // regist()
+    //     from("timer:base?period=30000")
+    //         .routeId("traveling")
+    //         .setBody(simple("INSERT INTO age_table(age,name) values('18','anton')"))
+    //         .to("jdbc:dataSource")
+    //         .setBody(simple("select * from age_table"))
+    //         .to("jdbc:dataSource")
+    //         .log(">>>>>>>> \${body}")
+    //         }
+
     override fun configure() {
-       // regist()
-        from("timer:base?period=30000")
+        val objectMapper = ObjectMapper()
+            objectMapper.registerModule(KotlinModule())
+        var age=""
+        var name=""
+       // JacksonDataFormat(objectMapper,Person::class.java)
+        from("file:/Users/azaitsev/Downloads/files/input")
             .routeId("traveling")
-            .setBody(simple("INSERT INTO age_table(age) values(18)"))
-            //.log("\${headers.CamelFileName}")
-            .to("jdbc:dataSource")
-            .setBody(simple("select * from age_table"))
-            .to("jdbc:dataSource")
-            .log(">>>>>>>> \${body}")
+           // .log("\${headers.CamelFileName}")
+
+             .unmarshal(JacksonDataFormat(objectMapper,Person::class.java))
+             .process { e ->
+                age = e.getIn().getBody(Person::class.java).age
+               name = e.getIn().getBody(Person::class.java).name
+                // val sql = "INSERT INTO age_table(age,name) values('$age','$name')"
+                 // jdbcTemplate.queryForObject(sql, String::class.java)
+                 // e.getIn().body
+             }
+            // .unmarshal(JacksonDataFormat(objectMapper,String::class.java))
+            // .to("file:/Users/azaitsev/Downloads/files/output")
+             .setBody(simple("INSERT INTO age_table(age,name) values('$age','$name')"))
+            .log(">>>>> \${body}")
+             .to("jdbc:dataSource")
+            .end()
+            // .setBody(simple("select * from age_table"))
+            // .to("jdbc:dataSource")
             }
+
+    data class Person(
+        val age: String,
+        val name: String,
+    )
 }
+
+// fun main(){
+//     val  str = String(Files.readAllBytes(Paths.get("/Users/azaitsev/Downloads/files/input/test.json")))
+//     val objectMapper = ObjectMapper()
+//     objectMapper.registerModule(KotlinModule())
+//     println(objectMapper.readValue(str,Starter.Person::class.java))
+// }
